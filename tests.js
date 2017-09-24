3c862971451b212f4b24cccd125cd3f180669b8f
Kim Morsha D. Zaldua <kdzaldua@up.edu.ph>
2017-09-24 20:12:28 +0800

    unit test case for getScheme() function

--- tests.js
+++ tests.js
@@ -1,6 +1,33 @@
 chai.config.includeStack = false;
 chai.config.truncateThreshold = 0;
 
+describe('Scheme checker', function() {
+  it('should return correct scheme', function() {
+    var validurl = 'http://jack:blarney@www.foo.com/bar/foo/acme?stuff=thing&foo=bar&blarg=snoggle#place';
+    var scheme = getScheme(validurl);
+
+    chai.assert.equal(scheme, "http");
+  });
+   it('should return correct scheme', function() {
+    var validurl = 'https://domain.com?poo=javascript%20decode%20uri%20%2B%20sign%20to%20space';
+    var scheme = getScheme(validurl);
+
+    chai.assert.equal(scheme, "https");  
+  });
+   it('should return correct scheme', function() {
+    var validurl = 'file:///home/arnelle/Documents/example.txt';
+    var scheme = getScheme(validurl);
+
+    chai.assert.equal(scheme, "file");  
+  });
+   it('should return correct scheme', function() {
+    var validurl = 'mailto:arnellebalane@gmail.com';
+    var scheme = getScheme(validurl);
+
+    chai.assert.equal(scheme, "mailto");  
+  });
+});
+
 describe('Port checker', function() {
   it('getPort() should return correct port if scheme uses default port.', function() {
     var validurl = 'http://jack:blarney@www.foo.com/bar/foo/acme?stuff=thing&foo=bar&blarg=snoggle#place';
@@ -24,6 +51,7 @@ describe('Port checker', function() {
     chai.assert.equal(port, null);
   });
 });
+
 describe('Path checker', function() {
   it('should return correct path', function() {
     var validurl = 'http://jack:blarney@www.foo.com/bar/foo/acme?stuff=thing&foo=bar&blarg=snoggle#place';
