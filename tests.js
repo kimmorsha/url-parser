76f6758600dd99bb90cd9d7be929fb6f8e373c79
arpacada <arpacada@up.edu.ph>
2017-09-24 17:31:36 +0800

    Unit tests for getPort() and getPath()

--- tests.js
+++ tests.js
@@ -1,60 +1,42 @@
 chai.config.includeStack = false;
 chai.config.truncateThreshold = 0;
 
-var testCases = {
-  'http://user:pass@foo:21/bar#c': 'http|user|pass|foo|21|/bar|^|c',
-  'http://f:00000000000000/c': 'http|^|^|f|0|/c|^|^',
-  'http://foo.com:b@d/': 'http|foo.com|b|d|80|/|^|^',
-  'foo:/': 'foo|^|^|^|^|/|^|^',
-  'madeupscheme:/example.com/': 'madeupscheme|^|^|^|^|/example.com/|^|^',
-  'http://www.domain.com/first/second/?test=foo': 'http|^|^|www.domain.com|80|/first/second/|test=foo|^',
-  'http://www.domain.com/path#anchor': 'http|^|^|www.domain.com|80|/path|^|anchor',
-  'http://atpass:foo%40bar@127.0.0.1:8080/path?search=foo#bar': 'http|atpass|foo@bar|127.0.0.1|8080|/path|search=foo|bar',
-  'http://user:pass@host.com:81/directory/file.ext?query=1#anchor': 'http|user|pass|host.com|81|/directory/file.ext|query=1|anchor',
-  'https://example.com/subjects?name=Web%20Engineering': 'https|^|^|example.com|443|/subjects|name=Web Engineering|^',
-  'http://domain.com?poo=javascript%20decode%20uri%20%2B%20sign%20to%20space': 'http|^|^|domain.com|80||poo=javascript decode uri + sign to space|^',
-'http://www.domain.com/#!/first/second/?test=foo': 'http|^|^|www.domain.com|80|/|^|!/first/second/?test=foo',
-'file:///home/root/docs/dirs/directory%20with%20spaces': 'file|^|^|^|^|/home/root/docs/dirs/directory with spaces|^|^',
-'http://localhost#why=do&this': 'http|^|^|localhost|80||^|why=do&this',
-'http://jack:blarney@www.foo.com/bar/foo/acme?stuff=thing&foo=bar&blarg=snoggle#place': 'http|jack|blarney|www.foo.com|80|/bar/foo/acme|stuff=thing,foo=bar,blarg=snoggle|place',
-'http://u:p@0.0.0.0:1001/a/b/c?one=1&two=2#three': 'http|u|p|0.0.0.0|1001|/a/b/c|one=1,two=2|three',
-'ftp://example.com/': 'ftp|^|^|example.com|21|/|^|^'
-};
+describe('Port checker', function() {
+  it('getPort() should return correct port if scheme uses default port.', function() {
+    var validurl = 'http://jack:blarney@www.foo.com/bar/foo/acme?stuff=thing&foo=bar&blarg=snoggle#place';
+    var port = getPort(validurl)
 
+    chai.assert.equal(port, 80);
+  });
+   it('getPort() should return correct port if it is explicitly shown in the url.', function() {
+    var url = 'http://atpass:foo%40bar@127.0.0.1:8080/path?search=foo#bar';
+   
 
-function assert(url, parsed) {
-  parsed = parsed.split(/\|/g);
-  var expected = {
-    scheme: normalize(parsed[0]),
-    authority: {
-      username: normalize(parsed[1]),
-      password: normalize(parsed[2]),
-      host: normalize(parsed[3]),
-      port: normalize(parsed[4])
-    },
-    path: normalize(parsed[5]),
-    query: parsed[6] === '^' ? null : parsed[6].split(/,/g).reduce(function(queries, query) {
-      var pair = query.split('=');
-      queries[pair[0]] = pair[1];
-      return queries;
-    }, {}),
-    fragment: normalize(parsed[7])
-  };
-  return chai.expect(parse(url)).to.deep.equal(expected);
-}
+    var port = getPort(url);
 
+    chai.assert.equal(port, 8080);
+  });
+   it('getPort() should return "null" if host is not present', function() {
+    var url = 'madeupscheme:/example.com/';
+   
+    var port = getPort(url);
 
-function normalize(value) {
-  return value === '^' ? null : value;
-}
+    chai.assert.equal(port, null);
+  });
+});
+describe('Path checker', function() {
+  it('should return correct path', function() {
+    var validurl = 'http://jack:blarney@www.foo.com/bar/foo/acme?stuff=thing&foo=bar&blarg=snoggle#place';
+    var path = getPath(validurl)
 
+    chai.assert.equal(path, '/bar/foo/acme');
+  });
+   it('should return an empty string if path is not present', function() {
+    var url = 'http://domain.com?poo=javascript%20decode%20uri%20%2B%20sign%20to%20space';
+   
 
-describe('URL Parser', function() {
-  for (var key in testCases) {
-    it(key, (function(key) {
-      return function() {
-        assert(key, testCases[key]);
-      };
-    })(key));
-  }
+    var path = getPath(url);
+
+    chai.assert.equal(path, "");
+  });
 });
