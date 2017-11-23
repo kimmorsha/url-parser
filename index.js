77d010359089a9cd60cd0371c538dda7480471e1
arpacada <arpacada@up.edu.ph>
2017-11-23 16:00:05 +0800

    added Browserify to use require()

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ app.set('views', './templates');
 app.get('/', function (request, response) { 
    response.render('index.html');
 });
-app.get('/unittest', function (request, response) {
-	response.render('unittest.html');
-});
 app.listen(3000, function () {
 	console.log('Server is now listening at port 3000');
 });
